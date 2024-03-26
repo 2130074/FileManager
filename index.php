7e9fbce5db120cdac630feec0ff6a871d9973ac5
2130074 <2130074@upv.edu.mx>
2024-03-25 20:10:39 -0600

    Listar archivos funcional

--- index.php
+++ index.php
@@ -5,20 +5,35 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Manejador de Archivos</title>
   <link rel="stylesheet" href="./style.css">
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      document.querySelector(".boton-listar").addEventListener("click", function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "listar_archivos.php", true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector(".lista-archivos").innerHTML = xhr.responseText;
+          }
+        };
+        xhr.send();
+      });
+    });
+  </script>
+
 </head>
+
 <body>
-<div class="contenedor-transparente">
-<h2>¡Bienvenida/o!</h2> 
-
-<div class="botones-container">
-    <button class="boton-listar">Listar archivos</button>
-    <button class="boton-mostrar">Mostrar archivos</button>
-    <button class="boton-subir">Subir archivos</button>
-    <button class="boton-borrar">Borrar archivos</button>
- </div>
-
- <button class="boton-salir" onclick="location.href='./login.php'">Cerrar sesión</button>
-
-
+  <div class="contenedor-transparente">
+    <h2>¡Bienvenida/o!</h2>
+    <div class="botones-container">
+      <button class="boton-listar">Listar archivos</button>
+      <button class="boton-mostrar">Mostrar archivos</button>
+      <button class="boton-subir">Subir archivos</button>
+      <button class="boton-borrar">Borrar archivos</button>
+    </div>
+    <div class="lista-archivos"></div>
+    <button class="boton-salir" onclick="location.href='./login.php'">Cerrar sesión</button>
+  </div>
 </body>
+
 </html>
--- user.php
+++ user.php
@@ -5,6 +5,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Manejador de Archivos</title>
   <link rel="stylesheet" href="./styless.css">
+
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      document.querySelector(".boton-listar").addEventListener("click", function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "listar_archivos.php", true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector(".lista-archivos").innerHTML = xhr.responseText;
+          }
+        };
+        xhr.send();
+      });
+    });
+  </script>
+
 </head>
 <body>
 <div class="contenedor-transparente">
@@ -14,7 +30,7 @@
     <button class="boton-listar">Listar archivos</button>
     <button class="boton-mostrar">Mostrar archivos</button>
  </div>
-
+ <div class="lista-archivos"></div>
  <button class="boton-salir" onclick="location.href='./login.php'">Cerrar sesión</button>
 
 
